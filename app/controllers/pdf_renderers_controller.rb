c2acadcf3d7927312a94ea977dc4f8473fdfde53
saiyencoder <jsantizo20@gmail.com>
2017-10-30 20:01:52 -0500

    Test pdf

--- app/controllers/pdf_renderers_controller.rb
+++ app/controllers/pdf_renderers_controller.rb
@@ -1,7 +1,9 @@
 class PdfRenderersController < ApplicationController
 
   def show
-    @resume = Resume.find(params[:id])
+    pdf = Prawn::Document.new
+    pdf.text "Monkeys IQ > Victor IQ"
+    send_data pdf.render, :filename => "Monkeys.pdf", :type => "application/pdf"
   end
 
 end
