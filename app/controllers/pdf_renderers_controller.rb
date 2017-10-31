39a0c07c1214d11587010343acbf9cc1be749ba6
MrFuentastic <vfuentes8587@gmail.com>
2017-10-30 20:18:25 -0500

    added unirest gem and started api request stuff

--- app/controllers/pdf_renderers_controller.rb
+++ app/controllers/pdf_renderers_controller.rb
@@ -1,7 +1,13 @@
 class PdfRenderersController < ApplicationController
 require "prawn"
+require 'unirest'
 
   def show
+    response = Unirest.patch("#{ ENV['HOST_NAME']}.json",
+                headers:{"Accept" => "application/json"},
+                parameters: {}
+                ).body
+
     @student = Student.find(params[:id])
 
     pdf = Prawn::Document.new
