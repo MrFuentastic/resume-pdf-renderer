9f638f31edc060974bd9701f7e14a50e7158f49b
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-10-30 19:46:24 -0500

    added prawn data

--- app/controllers/pdf_renderers_controller.rb
+++ app/controllers/pdf_renderers_controller.rb
@@ -1,7 +1,12 @@
 class PdfRenderersController < ApplicationController
+require "prawn"
 
   def show
+    @student = Student.find(params[:id])
 
+    pdf = Prawn::Document.new
+    pdf.text "Hello World"
+    pdf.render_file "assignment.pdf"
   end
 
 end
