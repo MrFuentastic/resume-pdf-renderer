fea2902418e9c1251c1c5de8a38d82cc54026ed0
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-10-31 18:07:38 -0500

    pushing so I can pull

--- app/controllers/pdf_renderers_controller.rb
+++ app/controllers/pdf_renderers_controller.rb
@@ -2,8 +2,6 @@ class PdfRenderersController < ApplicationController
 require "prawn"
 require 'unirest'
 
-<<<<<<< HEAD
-=======
   def show
     # response = Unirest.patch("#{ ENV['HOST_NAME']}.json",
     #             headers:{"Accept" => "application/json"},
@@ -28,6 +26,4 @@ require 'unirest'
     pdf.text "Hello World"
     pdf.render_file "assignment.pdf"
   end
-
->>>>>>> 69a89133fc748c9419c13b8a39c4b0513e4ddfdd
 end
