8f8f5c35731b32eef471e4e0900ae1b49a6bee8f
MrFuentastic <vfuentes8587@gmail.com>
2017-10-31 21:11:23 -0500

    continued working on wacky pdf

--- app/controllers/pdf_renderers_controller.rb
+++ app/controllers/pdf_renderers_controller.rb
@@ -1,6 +1,7 @@
 class PdfRenderersController < ApplicationController
 require "prawn"
 require 'unirest'
+HEADERS = {'X-User-Email' => ENV['api_email'], 'Authorization' => "Token token=#{ENV['api_token']}", "Accept" => "application/json" }
 
   def dope
     response = Unirest.get("https://crypto-currents-squidshack.herokuapp.com/api/v1/students.json",
@@ -19,12 +20,11 @@ require 'unirest'
 
 
   def wacky
-    HEADERS = {'X-User-Email' => ENV['API_EMAIL'], 'Authorization' => "Token token=#{ENV['API_KEY']}", "Accept" => "application/json" }
+
     
     response = Unirest.get("https://crypto-currents-squidshack.herokuapp.com/api/v1/students.json",
-                          headers: HEADERS,
+                          headers: HEADERS
                           ).body
-
     name = "#{response[0]["first_name"]} #{response[0]["last_name"]}"
     email = "#{response[0]["email"]}"
     phone_number = "#{response[0]['phone_number']}"
@@ -33,30 +33,32 @@ require 'unirest'
     educations = response[0]["educations"]
     skills = response[0]["skills"]
     experiences = response[0]["experiences"]
-    text_color = rand(256)
-    background_color = rand(256)
+    text_color = [rand(100), rand(100), rand(100), rand(100)]
+    background_color = [rand(100), rand(100), rand(100), rand(100)]
     
 
     pdf = Prawn::Document.new
-    pdf.text name, :color => "D4D4D4"
-    pdf.text email
-    pdf.text phone_number
 
-    experiences.each do |job|
-      pdf.text job["company_name"]
-      pdf.text job["job_title"]
-      pdf.text job["details"]
-    end
+      pdf.text name, :color => text_color
+      pdf.text linkedin, 
+      pdf.text email, :color => text_color
+      pdf.text phone_number, :color => text_color
 
-    educations.each do |school|
-      pdf.text school["university"]
-      pdf.text school["degree"]
-      pdf.text school["details"]
-    end
+      experiences.each do |job|
+        pdf.text job["company_name"], :color => text_color
+        pdf.text job["job_title"], :color => text_color
+        pdf.text job["details"], :color => text_color
+      end
 
-    skills.each do |skill|
-      pdf.text skill["skill_name"]
-    end
+      educations.each do |school|
+        pdf.text school["university"], :color => text_color
+        pdf.text school["degree"], :color => text_color
+        pdf.text school["details"], :color => text_color
+      end
+
+      skills.each do |skill|
+        pdf.text skill["skill_name"], :color => text_color
+      end
     
     send_data pdf.render, :type => "application/pdf", disposition: "inline"
   end
