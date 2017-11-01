6fa16e082d17712be0a52466a58f94221ced9189
saiyencoder <jsantizo20@gmail.com>
2017-10-31 21:18:24 -0500

    prof

--- app/controllers/pdf_renderers_controller.rb
+++ app/controllers/pdf_renderers_controller.rb
@@ -2,23 +2,18 @@ class PdfRenderersController < ApplicationController
 require "prawn"
 require 'unirest'
 
+HEADERS = {'X-User-Email' => ENV['API_EMAIL'], 'Authorization' => "Token token=#{ENV['API_KEY']}", "Accept" => "application/json" }
+
   def dope
 
 
   end
 
 
-  def show
-    
-
-  end
-
-
   def wacky
-    HEADERS = {'X-User-Email' => ENV['API_EMAIL'], 'Authorization' => "Token token=#{ENV['API_KEY']}", "Accept" => "application/json" }
     
     response = Unirest.get("https://crypto-currents-squidshack.herokuapp.com/api/v1/students.json",
-                          headers: HEADERS,
+                          headers: HEADERS
                           ).body
 
     name = "#{response[0]["first_name"]} #{response[0]["last_name"]}"
@@ -58,10 +53,9 @@ require 'unirest'
   end
 
   def prof
-    HEADERS = {'X-User-Email' => ENV['API_EMAIL'], 'Authorization' => "Token token=#{ENV['API_KEY']}", "Accept" => "application/json" }
     
     response = Unirest.get("https://crypto-currents-squidshack.herokuapp.com/api/v1/students.json",
-                          headers: HEADERS,
+                          headers: HEADERS
                           ).body
 
     name = "#{response[0]["first_name"]} #{response[0]["last_name"]}"
@@ -77,18 +71,19 @@ require 'unirest'
     
 
     pdf = Prawn::Document.new
-    pdf.text name, :color => "D4D4D4"
+    pdf.text name, :align => :center, :style => :bold, :font_size => 16
+    pdf.stroke_horizontal_rule
     pdf.text email
     pdf.text phone_number
 
     experiences.each do |job|
-      pdf.text job["company_name"]
+      pdf.text job["company_name"], :style => :bold
       pdf.text job["job_title"]
       pdf.text job["details"]
     end
 
     educations.each do |school|
-      pdf.text school["university"]
+      pdf.text school["university"], :style => :bold
       pdf.text school["degree"]
       pdf.text school["details"]
     end
